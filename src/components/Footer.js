f9cb27409ba58d75ff1c847bda2a1437fb74929c
Shivam8051 <shivamgupta7451893588@gmail.com>
2022-07-14 02:03:39 +0530

    deployed

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Made By Shivam Gupta!!!</p>
+    </div>
+  );
+};
+
+export default Footer;
